0d7611016170ecd13f012dbc8a11f0e0597d9c33
Bianca Pereira <bcpereira@Bianca.local>
2019-10-01 16:35:09 -0600

    modified fethPostswithquery actionCreator to take in an optional callback

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,7 +5,6 @@ export function fetchRecentPosts() {
   return function(dispatch) {
     //perform our request in here
     axios.get("https://api.dailysmarty.com/posts").then(response => {
-      console.log(response.data.posts);
       dispatch({
         type: SET_RECENT_POSTS,
         payload: response.data.posts
@@ -14,17 +13,19 @@ export function fetchRecentPosts() {
   };
 }
 
-export function fetchPostsWithQuery(query) {
+export function fetchPostsWithQuery(query, callback) {
   return function(dispatch) {
     //perform our request in here
     axios
       .get(`https://api.dailysmarty.com/search?q=${query}`)
       .then(response => {
-        console.log(response.data.posts);
         dispatch({
           type: SET_RESULTS_POSTS,
           payload: response.data.posts
         });
+        if (callback) {
+          callback();
+        }
       });
   };
 }
