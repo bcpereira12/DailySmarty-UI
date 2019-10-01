1aba99935703654adf34c9ec5b6fdac4d0d4994e
Bianca Pereira <bcpereira@Bianca.local>
2019-10-01 13:38:51 -0600

    connected RecentPosts to redux and setup redux-thunk for api requests

--- src/components/recentPosts.js
+++ src/components/recentPosts.js
@@ -1,6 +1,12 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import * as actions from "../actions";
 
-export default class RecentPosts extends Component {
+class RecentPosts extends Component {
+  componentDidMount() {
+    this.props.fetchRecentPosts();
+  }
+
   render() {
     return (
       <div className="recent-posts">
@@ -18,3 +24,8 @@ export default class RecentPosts extends Component {
     );
   }
 }
+
+export default connect(
+  null,
+  actions
+)(RecentPosts);
