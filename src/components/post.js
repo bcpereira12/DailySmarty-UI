beb51a40fe561be854da14edf285d4c26669cf8e
Bianca Pereira <bcpereira@Bianca.local>
2019-10-01 20:44:18 -0600

    finished styling results component

--- src/components/post.js
+++ src/components/post.js
@@ -45,16 +45,14 @@ class Post extends Component {
       );
     } else {
       return (
-        <li className="result-post">
+        <li
+          className="result-post"
+          onMouseEnter={() => this.setState({ height: 70 })}
+          onMouseLeave={() => this.setState({ height: 0 })}
+        >
           <div className="result-post-topics">{this.renderTopics()}</div>
           <div className="results-post-title">
-            <a
-              href={this.props.url_for_post}
-              onMouseEnter={() => this.setState({ height: 70 })}
-              onMouseLeave={() => this.setState({ height: 0 })}
-            >
-              {this.props.title}
-            </a>
+            <a href={this.props.url_for_post}>{this.props.title}</a>
           </div>
           <AnimateHeight duration={500} height={this.state.height}>
             <div className="result-post-links">{this.renderLinks()}</div>
